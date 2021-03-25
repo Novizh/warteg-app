407d8582770c5001bacb4a84757a85089d19d0d9
tianmarillio <tiansyah.marillio@gmail.com>
2021-03-26 00:20:48 +0700

    dynamic hampir done

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,29 @@ const items = [
   { name: 'kikil' },
   { name: 'capcay' },
   { name: 'mie-goreng' },
+  { name: 'ayam-goreng' },
+  { name: 'ayam-bakar' },
+  { name: 'ayam-krispi' },
+  { name: 'ayam-geprek' },
 ]
 
 let cartTable = document.querySelector('#cart')
 
-function createRow() {
-  let row = document.querySelector('.row')
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function createFoodCard() {
+function createFoodCard(name) {
   let row = document.querySelector('.row')
 
   let cardContainer = document.createElement('div')
   cardContainer.setAttribute('class', 'col-xl-3 col-md-6 mb-4')
 
   let cardBorder = document.createElement('div')
-  cardBorder.setAttribute('class', 'card border-left-primary shadow h-100 py-2')
+  if (colorCounter % 4 === 0) cardBorder.setAttribute('class', 'card border-left-primary shadow h-100 py-2') // buat dinamis
+  if (colorCounter % 4 === 3) cardBorder.setAttribute('class', 'card border-left-success shadow h-100 py-2') // buat dinamis
+  if (colorCounter % 4 === 2) cardBorder.setAttribute('class', 'card border-left-info shadow h-100 py-2') // buat dinamis
+  if (colorCounter % 4 === 1) cardBorder.setAttribute('class', 'card border-left-warning shadow h-100 py-2') // buat dinamis
 
   let cardBody = document.createElement('div')
   cardBody.setAttribute('class', 'card-body')
@@ -29,9 +36,9 @@ function createFoodCard() {
 
   // isi cardBody
   let itemName = document.createElement('div')
-  itemName.setAttribute('id', 'telor-dadar-item') // buat dinamis
+  itemName.setAttribute('id', `${name}-item`) // buat dinamis/
   itemName.setAttribute('class', 'text-l font-weight-bold text-primary text-uppercase mb-1')
-  itemName.innerHTML = 'Telor Dadar' //buat dinamis
+  itemName.innerHTML = name.split('-').join(' ') //buat dinamis/
 
   // pic
   let itemPic = document.createElement('div')
@@ -40,10 +47,10 @@ function createFoodCard() {
   let itemPicInner = document.createElement('div')
   itemPicInner.setAttribute('class', 'col mr-2')
 
-  let imgData = document.createElement('img') //buat dinamis
+  let imgData = document.createElement('img') 
   imgData.setAttribute('class', 'img-thumbnail rounded mx-auto') 
-  imgData.setAttribute('src', './assets/telur-dadar-warteg.jpg')
-  imgData.setAttribute('alt', 'telor dadar')
+  imgData.setAttribute('src', `./assets/${name}.jpg`) // buat dinamis/
+  imgData.setAttribute('alt', `${name}`) // buat dinamis/
 
   itemPicInner.appendChild(imgData)
   itemPic.appendChild(itemPicInner)
@@ -51,44 +58,23 @@ function createFoodCard() {
   //
   let itemPrice = document.createElement('div')
   itemPrice.setAttribute('class', 'p p-3 font-weight-bold text-gray-800')
-  itemPrice.innerHTML = 'Rp 5.000' //buat dinamis
+  itemPrice.innerHTML = 'Rp 5.000' // buat dinamis
 
   let itemButton = document.createElement('div')
-  itemButton.setAttribute('id', 'telor-dadar-button') // buat dinamis
+  itemButton.setAttribute('id', `${name}-button`) // buat dinamis/
   itemButton.setAttribute('class', 'btn btn-success btn-icon-split float-right')
-  itemButton.innerHTML = 'Pesan'
+  
+  let itemButtonText = document.createElement('span')
+  itemButtonText.setAttribute('class', 'text')
+  itemButtonText.innerHTML = 'Pesan'
+  
+  itemButton.appendChild(itemButtonText)
 
   cardBody.appendChild(itemName)
   cardBody.appendChild(itemPic)
   cardBody.appendChild(itemPrice)
   cardBody.appendChild(itemButton)
 }
-
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-createFoodCard()
-
 
 function addItems(itemName, itemButton) {
   let item = document.querySelector(itemName)
@@ -97,14 +83,16 @@ function addItems(itemName, itemButton) {
   button.addEventListener('click', function (event) {
     let cartRow = document.createElement('tr')
     let cartItem = document.createElement('td')
-    cartItem.innerHTML = item.innerHTML
-    console.log(cartItem.innerHTML)
+    cartItem.innerHTML = item.innerHTML.toUpperCase()
 
     cartTable.appendChild(cartRow)
     cartRow.appendChild(cartItem)
   })
 }
 
+let colorCounter = 0
 for (let i = 0; i < items.length; i++) {
+  createFoodCard(items[i].name)
   addItems(`#${items[i].name}-item`, `#${items[i].name}-button`)
+  colorCounter++
 }

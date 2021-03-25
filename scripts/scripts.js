b5dc2c79e951818e288d891a516918582c2085f7
tianmarillio <tiansyah.marillio@gmail.com>
2021-03-25 23:39:03 +0700

    createFoodCart() done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,13 +13,13 @@ function createRow() {
 
 function createFoodCard() {
   let row = document.querySelector('.row')
-  
+
   let cardContainer = document.createElement('div')
   cardContainer.setAttribute('class', 'col-xl-3 col-md-6 mb-4')
 
   let cardBorder = document.createElement('div')
   cardBorder.setAttribute('class', 'card border-left-primary shadow h-100 py-2')
-  
+
   let cardBody = document.createElement('div')
   cardBody.setAttribute('class', 'card-body')
 
@@ -27,42 +27,63 @@ function createFoodCard() {
   cardContainer.appendChild(cardBorder)
   cardBorder.appendChild(cardBody)
 
-  // // isi cardBody
-  // let itemName = document.createElement('div')
-  // itemName.setAttribute('id', 'telor-dadar-item') // buat dinamis
-  // itemName.setAttribute('class', 'text-l font-weight-bold text-primary text-uppercase mb-1')
-  // itemName.innerHTML = 'Telor Dadar' //buat dinamis
+  // isi cardBody
+  let itemName = document.createElement('div')
+  itemName.setAttribute('id', 'telor-dadar-item') // buat dinamis
+  itemName.setAttribute('class', 'text-l font-weight-bold text-primary text-uppercase mb-1')
+  itemName.innerHTML = 'Telor Dadar' //buat dinamis
 
-  // // pic
-  // let itemPic = document.createElement('div')
-  // itemPic.setAttribute('class', 'row no-gutters align-items-center')
-  
-  // let itemPicInner = document.createElement('div')
-  // itemPicInner.setAttribute('class', 'col mr-2')
-  
-  // let imgData = document.createElement('img') //buat dinamis
-  // imgData.setAttribute('class', 'img-thumbnail rounded mx-auto') 
-  // imgData.setAttribute('src', './assets/telur-dadar-warteg.jpg')
-  // imgData.setAttribute('alt', 'telor dadar')
-  
-  // itemPicInner.appendChild(imgData)
-  // itemPic.appendChild(itemPicInner)
-  
-  // //
-  // let itemPrice = document.createElement('div')
-  // itemPrice.setAttribute('class', 'p p-3 font-weight-bold text-gray-800')
-  // itemPrice.innerHTML = Rp 5.000 //buat dinamis
+  // pic
+  let itemPic = document.createElement('div')
+  itemPic.setAttribute('class', 'row no-gutters align-items-center')
 
-  // let itemButton = document.createElement('div')
-  // itemName.setAttribute('id', 'telor-dadar-button') // buat dinamis
-  // itemButton.setAttribute('class', 'btn btn-success btn-icon-split float-right')
+  let itemPicInner = document.createElement('div')
+  itemPicInner.setAttribute('class', 'col mr-2')
 
-  // cardBody.appendChild(itemName)
-  // cardBody.appendChild(itemPic)
-  // cardBody.appendChild(itemPrice)
-  // cardBody.appendChild(itemButton)
+  let imgData = document.createElement('img') //buat dinamis
+  imgData.setAttribute('class', 'img-thumbnail rounded mx-auto') 
+  imgData.setAttribute('src', './assets/telur-dadar-warteg.jpg')
+  imgData.setAttribute('alt', 'telor dadar')
+
+  itemPicInner.appendChild(imgData)
+  itemPic.appendChild(itemPicInner)
+
+  //
+  let itemPrice = document.createElement('div')
+  itemPrice.setAttribute('class', 'p p-3 font-weight-bold text-gray-800')
+  itemPrice.innerHTML = 'Rp 5.000' //buat dinamis
+
+  let itemButton = document.createElement('div')
+  itemButton.setAttribute('id', 'telor-dadar-button') // buat dinamis
+  itemButton.setAttribute('class', 'btn btn-success btn-icon-split float-right')
+  itemButton.innerHTML = 'Pesan'
+
+  cardBody.appendChild(itemName)
+  cardBody.appendChild(itemPic)
+  cardBody.appendChild(itemPrice)
+  cardBody.appendChild(itemButton)
 }
 
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
+createFoodCard()
 createFoodCard()
 createFoodCard()
 createFoodCard()

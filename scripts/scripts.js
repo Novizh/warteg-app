1606fbb0d5851aadbae89c60bec63cc617fce881
garryfishy <bboyfishy@gmail.com>
2021-03-25 23:25:34 +0700

    testHarga

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,27 @@
-function addItems(itemName, itemButton, cart) {
+
+
+function addItems(itemName, itemButton, itemPrice, cart) {
   let item = document.querySelector(itemName)
   let button = document.querySelector(itemButton)
   let cartTable = document.querySelector(cart)
+  let price = document.querySelector(itemPrice)
 
   button.addEventListener('click', function (event) {
     let cartRow = document.createElement('tr')
     let cartItem = document.createElement('td')
+    let cartPrice = document.createElement('td')
+
+    cartPrice.innerHTML = price.innerHTML
     cartItem.innerHTML = item.innerHTML
-    console.log(cartItem.innerHTML)
 
     cartTable.appendChild(cartRow)
     cartRow.appendChild(cartItem)
+    cartRow.appendChild(cartPrice)
   })
 }
 
-addItems('#telor-dadar-item', '#telor-dadar-button', '#cart')
-addItems('#kikil-item', '#kikil-button', '#cart')
-addItems('#capcay-item', '#capcay-button', '#cart')
-addItems('#mie-goreng-item', '#mie-goreng-button', '#cart')
+
+addItems('#telor-dadar-item', '#telor-dadar-button', '#hargaTelor', '#cart')
+addItems('#kikil-item', '#kikil-button','#hargaKikil', '#cart')
+addItems('#capcay-item', '#capcay-button','#hargaCapcay' ,'#cart')
+addItems('#mie-goreng-item', '#mie-goreng-button','#hargaMie', '#cart')
